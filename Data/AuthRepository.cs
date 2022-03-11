7dc3535b8121571a4fd4d460cf17680685cf6386
Mirza Hamzic <mirzaha@maestralsolutions.com>
2022-03-11 13:44:12 +0100

    added JWT

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -1,17 +1,30 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using norm_calc.Models;
 using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace norm_calc.Data
 {
     public class AuthRepository : IAuthRepository
     {
+        //access to DB
         private readonly AppDbContext _context;
 
-        public AuthRepository(AppDbContext context)
+        //access to setting in json file
+        private readonly IConfiguration _configuration;
+
+        public AuthRepository(AppDbContext context, IConfiguration configuration)
         {
+            //access to DB
             _context = context;
+
+            //access to setting in json file
+            _configuration = configuration;
         }
         public async Task<ServiceResponse<string>> Login(string name, string password)
         {
@@ -34,7 +47,7 @@ namespace norm_calc.Data
             }
             else
             {
-                response.Data = user.Id.ToString();
+                response.Data = CreateToken(user);
             }
             return response;
 
@@ -94,6 +107,38 @@ namespace norm_calc.Data
                 }
                 return true;
             }
+        }
+
+        private string CreateToken(User user)
+        {
+            // elements we encode in token
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Name)
+            };
+
+            // symetric key we added in json file
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+
+            // signing the key and signin credentials
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            //creating token and adding options like expires date and claims
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = credentials
+
+            };
+
+            //token handler for JWT to create security token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
